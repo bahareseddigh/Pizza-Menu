6a2e49efd88b2211cae9e0adca6014dbb839a2a3
bahare <pondblue07@gmail.com>
2023-12-07 23:14:26 +0330

    set style on App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
+import "../src/App.css";
 
 export default function App() {
   return (
-    <>
+    <main className="container">
       <Header />
       <Menu />
       <Footer />
-    </>
+    </main>
   );
 }
